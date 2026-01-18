c47372e521b33e16009942c2bdd587fa114c0785
wisemantransports-ux <wisemantransports@gmail.com>
2026-01-18 07:20:21 +0200

    Refactor AdminLayout to remove Sidebar
    Removed Sidebar component and adjusted layout structure.

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,17 +1,15 @@
 "use client";
 
 import "@/globals.css";
-import Sidebar from "@/components/Sidebar";
 import Topbar from "@/components/Topbar";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-background">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Topbar />
-        <main className="flex-1 overflow-y-auto p-6 lg:p-8">{children}</main>
-      </div>
+    <div className="min-h-screen bg-background flex flex-col">
+      <Topbar />
+      <main className="flex-1 p-6 lg:p-8">
+        {children}
+      </main>
     </div>
   );
 }
